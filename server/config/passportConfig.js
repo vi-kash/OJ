6a1db0a61262ee9c56c0d3ebaa13e7ad2e44e0f3
Vikash Kumar <ivikashshahi@gmail.com>
2024-07-21 03:46:45 +0530

    solve the issue with redirect URI

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
+            callbackURL: "https://backend.online-judge.site/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
@@ -48,7 +48,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "/auth/github/callback",
+            callbackURL: "https://backend.online-judge.site/auth/github/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
